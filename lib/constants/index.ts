57939f330a6451c920d9dcf86903c1eece8b65de
edsay12 <edvandearaujo2@hotmail.com>
2025-02-02 17:35:00 -0300

    feat: metadata da pagina

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Fast Store";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A store that lets you buy anything";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
